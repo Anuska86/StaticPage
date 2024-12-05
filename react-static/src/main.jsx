8badbe5bbf91ae903310c2e811b531c6ce4106f8
Anuska86 <anasrm86@gmail.com>
2024-12-05 16:24:31 +0100

    main

--- react-static/src/main.jsx
+++ react-static/src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Header() {
+  return (
+    <header>
+      <img src="src/imgs/react-logo.png" width="80px" alt="React logo" />
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>I am learning React because: </h1>
+      <ol>
+        <li>There is a lot of demand for work</li>
+        <li>Is maintained by Meta</li>
+        <li>Is used on thousands of apps</li>
+      </ol>
+    </main>
+  );
+}
+
+function Footer()
+{
+  return (
+    <footer>
+        <small>© 2024 Ana Sappia Rey development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer/>
+    </>
+  );
+}
+
+root.render(
+  <>
+    <Page />
+  </>
+);
